955dee3cb94b2df222ca5835fc54872854e4b768
Nicole Hawkey <n.laggner@gmx.at>
2023-05-29 15:57:07 +0700

    Starting to change app into Redux

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { searchRobots } from './reducers';
+import { createLogger } from 'redux-logger';
 import './index.css';
 import App from './containers/App';
 import reportWebVitals from './reportWebVitals';
 import "tachyons";
 
+const logger = createLogger();
+const store = createStore(searchRobots, applyMiddleware(logger));
 
 const rootNode = document.getElementById("root");
 const root = ReactDOM.createRoot(rootNode);
 
-root.render(<App />);
+root.render(
+    <Provider store={store} >
+        <App />
+    </Provider>);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
